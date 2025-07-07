a96beccc92dedd21b72ce08bddd8920a05d80978
top <test@ex.com>
2025-07-06 21:57:36 -1200

    fix: wallet form

--- src/providers/WalletConnectProvider.tsx
+++ src/providers/WalletConnectProvider.tsx
@@ -1,45 +1,26 @@
 "use client";
 
-import { useMemo } from 'react'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
-import { WalletModalProvider} from '@solana/wallet-adapter-react-ui'
-import {
-    LedgerWalletAdapter,
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    TorusWalletAdapter,
-  } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js'
+import '@rainbow-me/rainbowkit/styles.css';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { WagmiProvider, http } from 'wagmi';
+import { mainnet, polygon, arbitrum, base, optimism } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export const WalletConnectProvider = ({ children } : {children : any}) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.Devnet
-    const endpoint = useMemo(() => {
-        if (network === WalletAdapterNetwork.Devnet) {
-            // return 'https://rpc.hellomoon.io/00f4178d-d782-4d0e-ac29-02706daa7be2'
-            return 'https://api.devnet.solana.com/'
-        }
-        return clusterApiUrl(network)
-    }, [network])
+const config = getDefaultConfig({
+  appName: 'tokenFi',
+  projectId: '2b15be3f7b915bd10c6c697e176d3342', // get from https://cloud.walletconnect.com/
+  chains: [mainnet, polygon, arbitrum, base, optimism],
+  ssr: true,
+});
 
-    const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-            // new SolflareWalletAdapter({ network }),
-            // new TorusWalletAdapter(),
-            // new LedgerWalletAdapter(),
-        ],
-        [network]
-    );
+const queryClient = new QueryClient();
 
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-                    {children}
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    )
-}
+export const WalletConnectProvider = ({ children }: { children: React.ReactNode }) => (
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        {children}
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+);
